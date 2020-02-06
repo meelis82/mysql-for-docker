fb2830046ed779a916f04a7863d973cff731c1a2
Meelis Valgevali <meelis@fenomen.ee>
2020-02-06 10:30:57 +0200

    line endings

--- src/mysql/build/modules/build/files/tmp/mysql_install.sh
+++ src/mysql/build/modules/build/files/tmp/mysql_install.sh
@@ -1,1 +1,9 @@
-#!/bin/bashapt-get updatedebconf-set-selections <<< \  "mysql-community-server mysql-community-server/root-pass password tiger"debconf-set-selections <<< \  "mysql-community-server mysql-community-server/re-root-pass password tiger"debconf-set-selections <<< \  "mysql-community-server mysql-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)"DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+#!/bin/bash
+
+apt-get update
+
+debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password container"
+
+debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password container"
+debconf-set-selections <<< "mysql-community-server mysql-server/default-auth-override select Use Legacy Authentication Method (Retain MySQL 5.x Compatibility)"
+DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
